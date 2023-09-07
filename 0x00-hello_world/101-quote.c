16d019bbb79c495963e2705fcdf3ae5a3554c2ce
deathstroke-Assassin <moh2004amed@gmail.com>
2023-09-07 16:51:02 -0700

    f

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,7 @@
 */
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-printf("\n");
+  fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+printf(stderr=Anything,"\n");
 return (1);
 }
