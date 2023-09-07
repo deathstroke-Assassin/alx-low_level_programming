b8e4f8f385ebe8c256673a3c306f6f78f17e708c
deathstroke-Assassin <moh2004amed@gmail.com>
2023-09-07 16:37:29 -0700

    f

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
 * main -  prints the size of files
-* Return: 1 if exited properly
+* Return: 1
 */
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+fprintf(stderr)
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }
