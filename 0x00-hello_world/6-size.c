4656c245c7cfd821430ca40edf44f07632e349ae
deathstroke-Assassin <moh2004amed@gmail.com>
2023-09-07 15:42:01 -0700

    h

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main -  prints the phrase
+* Return: 0 if exited properly
+*/
+int main(void)
+{
+  printf("size of a char: d% byte(s)\n" sizeof(char));
+  printf("size of a int: d% byte(s)\n" sizeof(int));
+  printf("size of a short: d% byte(s)\n" sizeof(short));
+  printf("size of a long: d% byte(s)\n" sizeof(long));
+ return (0);
+}
