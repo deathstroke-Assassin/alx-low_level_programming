6b1012e6ae514cd050e8f50fb63b71374dd3f420
deathstroke-Assassin <moh2004amed@gmail.com>
2023-09-07 15:48:28 -0700

    h

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,8 +6,9 @@
 int main(void)
 {
   printf("size of a char: d% byte(s)\n", sizeof(char));
-  printf("size of a int: d% byte(s)\n", sizeof(int));
-  printf("size of a short: d% byte(s)\n", sizeof(short));
-  printf("size of a long: d% byte(s)\n", sizeof(long));
+  printf("size of an int: d% byte(s)\n", sizeof(int));
+  printf("size of a float: d% byte(s)\n", sizeof(float));
+  printf("size of a long int: d% byte(s)\n", sizeof(long int));
+  printf("size of a long long int: d% byte(s)\n", sizeof(long long int));
  return (0);
 }
