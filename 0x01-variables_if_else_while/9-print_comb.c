4586f1cdef2e80b3ab43eb8df798da7b0c571db9
deathstroke-Assassin <moh2004amed@gmail.com>
2023-09-08 19:18:42 -0700

    g

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,12 +12,12 @@ int n = 48;
 for (n = 48; n <= 57; n++)
 {
 putchar (n);
- if (n < 57)
-   {
-     putchr(44);
-     putchr(32);
-   }
- }
+if (n < 57)
+{
+putchar(44);
+putchar(32);
+}
+}
 putchar ('\n');
 return (0);
 }
