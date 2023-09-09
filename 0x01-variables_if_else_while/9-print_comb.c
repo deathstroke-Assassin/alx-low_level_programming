21dd1ebbbbe050456eb46da2d5650bdee7259529
deathstroke-Assassin <moh2004amed@gmail.com>
2023-09-08 19:17:16 -0700

    s

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - assigns a random number to int n everytime
+ * it executes, and prints it
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n = 48;
+for (n = 48; n <= 57; n++)
+{
+putchar (n);
+ if (n < 57)
+   {
+     putchr(44);
+     putchr(32);
+   }
+ }
+putchar ('\n');
+return (0);
+}
+
