663d31f98040136007edc28f68708049d16bb71b
deathstroke-Assassin <moh2004amed@gmail.com>
2023-09-12 14:47:08 -0700

    g

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _islower - checks for character
+ * @c: the character to check
+ * Return: 1 if c is lowercaseor uppercase, 0 otherwise
+ */
+int _isalpha(int c)
+{
+return (c >= 'a' && c <= 'z' && c >= 'A' && c >= 'Z');
+}
