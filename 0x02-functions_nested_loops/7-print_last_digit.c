b237fb2fa4a28bc9e443d340885b384e7de245c3
deathstroke-Assassin <moh2004amed@gmail.com>
2023-09-12 15:54:01 -0700

    g

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_last_digit - checks for character
+ * @x: the number to check
+ * Return: 1 if c is letter, 0 otherwise
+ */
+int print_last_digit(int x)
+{
+return (x % 10);
+}
