d8da4410defabf7ef7ace192ee39d1eb2d4655ff
deathstroke-Assassin <moh2004amed@gmail.com>
2023-09-12 13:30:49 -0700

    g

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main -   prints the alphabet, in lowercase, followed by a new line.
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void);
+{
+  char c ='a';
+  while (c <= 'z')
+    {
+      _putchar(c);
+      c++
+    }
+_putchar('\n');
+return (0);
+}
