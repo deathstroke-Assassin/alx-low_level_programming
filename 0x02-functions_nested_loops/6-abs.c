b11261f7733603f9f16d72f7384a086870eb370a
deathstroke-Assassin <moh2004amed@gmail.com>
2023-09-12 15:46:39 -0700

    h

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,10 +1,14 @@
 #include "main.h"
 /**
  * _abs - checks for character
- * @n: the number to check
+ * @x: the number to check
  * Return: 1 if c is letter, 0 otherwise
  */
 int _abs(int x)
 {
+  if (x < 0)
+{
+return (-x);
+}
 return (x);
 }
