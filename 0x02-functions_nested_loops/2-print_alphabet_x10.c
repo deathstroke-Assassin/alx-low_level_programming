35c68257d59978c8941ae660eb9071197af52a38
deathstroke-Assassin <moh2004amed@gmail.com>
2023-09-12 14:17:23 -0700

    alph

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,15 +5,15 @@
 void print_alphabet_x10(void)
 {
   char c = 'a';
-  int n = 0;
-  while (n <= 9)
+  int n = 1;
+  while (n <= 10)
     {
       while (c <= 'z')
 	{
 	  _putchar(c);
 	  c++;
+	  n++;
 	}
       _putchar('\n');
-      n++;
     }
 }
