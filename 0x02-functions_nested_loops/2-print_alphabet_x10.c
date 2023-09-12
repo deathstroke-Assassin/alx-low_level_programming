7210eb6c664395c92d29576c01fb96869f97e735
deathstroke-Assassin <moh2004amed@gmail.com>
2023-09-12 13:53:08 -0700

    alph

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 -   prints the alphabet, in lowercase.
+ */
+void print_alphabet_x10(void)
+{
+char c = 'a';
+int n = 0;
+while (n <= 9)
+{
+while (c <= 'z')
+{
+_putchar(c);
+c++;
+}
+_putchar('\n');
+}
+n++;
+}
