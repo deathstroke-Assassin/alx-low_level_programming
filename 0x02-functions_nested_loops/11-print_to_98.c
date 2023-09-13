8320838c76dcb5fb49a9e483d02868572d92f7b3
deathstroke-Assassin <moh2004amed@gmail.com>
2023-09-12 18:22:36 -0700

    jj

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,22 +11,22 @@ if (n < 98)
 {
 while (n < 98)
 {
-printf("%d, ",n);
+printf("%d, ", n);
 n++;
 }
-printf("%d\n",n);
+printf("%d\n", n);
 }
 else if (n == 98)
 {
-printf("%d",n);
+printf("%d\n", n);
 }
 else if (n > 98)
 {
 while (n > 98)
 {
-printf("%d, ",n);
+printf("%d, ", n);
 n--;
 }
-printf("%d\n",n);
+printf("%d\n", n);
 }
 }
