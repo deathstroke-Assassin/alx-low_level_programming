92ca41374ae5ae7e50bff7e36f1508ca6d9c5763
deathstroke-Assassin <moh2004amed@gmail.com>
2023-09-12 17:50:01 -0700

    hha

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_to_98 - checks for character
+ * @n: 1 if c is letter, 0 otherwise
+ * Return: 1 if c is letter, 0 otherwise
+ */
+void print_to_98(int n)
+{
+n = 0;
+while (n <= 98)
+{
+_putchar(n);
+n++;
+}
+_putchar('\n');
+}
