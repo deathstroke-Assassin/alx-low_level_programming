e8dbb5804f58ee0af64a1a14e83a33385b43d499
deathstroke-Assassin <moh2004amed@gmail.com>
2023-09-12 18:45:32 -0700

    jj

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * add - checks for character
+ * @y: 1 if c is letter, 0 otherwise
+ * @x: 1 if c is letter, 0 otherwise
+ * Return: 1 if c is letter, 0 otherwise
+ */
+int add(int x, int y)
+{
+int z;
+x = 1;
+y = 2;
+int v = 1;
+for (v < 50; v++)
+{
+z = x + y;
+printf("%d, ", z)
+}
+if (v = 50)
+{
+printf("%d", z)
+}
+printf("\n")
+return (z);
+}
