975f1d131088cebaaae2c1fa6181d286dc27bc2d
deathstroke-Assassin <moh2004amed@gmail.com>
2023-09-14 16:52:11 -0700

    A

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include"main.h"
+/**
+ * mul - checks for uppercase character
+ * @a: the character to be checked
+ * @b: the character to be checked
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int mul(int a, int b)
+{
+  return (a * b)
+}
