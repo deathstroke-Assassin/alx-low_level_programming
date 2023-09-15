0279c6475ec918978f6cc668c9d561908754bfc6
deathstroke-Assassin <moh2004amed@gmail.com>
2023-09-14 18:28:18 -0700

    ao

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_line - prints the numbers, from 0 to 9,
+ * followed by a new line
+ */
+void print_line(int n)
+{
+
+while (n > 0)
+{
+_putchar'_');
+}
+_putchar('\n');
+}
+
