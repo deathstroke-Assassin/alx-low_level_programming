f6b859c0e4357c344e4e6b16d1350ef285b156fb
deathstroke-Assassin <moh2004amed@gmail.com>
2023-09-14 19:43:24 -0700

    d

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio>
+#include <stdio.h>
 
 /**
  * main - prints a square, followed by a new line;
@@ -7,25 +7,30 @@
  */
 int main(void)
 {
-  int x, z, y
-    for (x = 1; x < 100 x++)
-      {
-	if (x = x / 3)
-	  {
-	    printf("Fizz")
-	  }
-	else if (x = x / 5)
-	  {
-	    printf("Buzz")
-	  }
-	else if (x = x / 15)
-	  {
-	    printf("FizzBuzz")
-	  }
-	else
-	  {
-	  printf("%d", x)
-	  }
-      }
-  printf("\n")
+int x;
+for (x = 1; x <= 100; x++)
+{
+if (x % 3 == 0 && x % 5 != 0)
+{
+printf(" Fizz");
+}
+else if (x % 5 == 0 && x % 3 != 0)
+{
+printf(" Buzz");
+}
+else if (x % 5 == 0 && x % 3 == 0)
+{
+printf(" FizzBuzz");
+}
+else if (x == 1)
+{
+printf(" %d", x);
+}
+else
+{
+printf(" %d", x);
+}
+}
+printf("\n");
+return (0);
 }
