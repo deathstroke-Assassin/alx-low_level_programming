002a27e1f02abdc8376bad13fa95f7e2ab5a71f7
deathstroke-Assassin <moh2004amed@gmail.com>
2023-09-14 18:31:12 -0700

    ao

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,13 +3,14 @@
 /**
  * print_line - prints the numbers, from 0 to 9,
  * followed by a new line
+ * @n:followed by a new line
  */
 void print_line(int n)
 {
 
 while (n > 0)
 {
-_putchar'_');
+_putchar('_');
 }
 _putchar('\n');
 }
