82bb5a64cbf291b59ced64f531aad3f743980abf
deathstroke-Assassin <moh2004amed@gmail.com>
2023-09-14 17:11:45 -0700

    Ay

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include"main.h"
+/**
+ * void print_numbers(void) - checks for uppercase character
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+void print_numbers(void)
+{
+int c;
+for (c = 0; c < 9; c++)
+{
+_putchar(c);
+}   
+_putchar('\n');
+return (0);
+}
