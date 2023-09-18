8c165067633910413dacedec26cb0ac44f523989
deathstroke-Assassin <moh2004amed@gmail.com>
2023-09-18 16:29:03 -0700

    ha

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,23 +9,19 @@
 void rev_string(char *s)
 {
 int start = 0;
-    int end = 0;
+int end = 0;
+while (s[end] != '\0')
+{
+end++;
+}
+end--;
+while (start < end)
+{
+char temp = s[start];
+s[start] = s[end];
+s[end] = temp;
 
-    // Find the length of the string
-    while (s[end] != '\0')
-    {
-        end++;
-    }
-    end--;
-
-    // Reverse the string
-    while (start < end)
-    {
-        char temp = s[start];
-        s[start] = s[end];
-        s[end] = temp;
-
-        start++;
-        end--;
-    }
+start++;
+end--;
+}
 }
