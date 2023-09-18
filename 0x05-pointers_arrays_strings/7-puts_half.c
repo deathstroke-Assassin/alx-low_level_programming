c97324947ca965f658dc07b5ec3444e5b8688538
deathstroke-Assassin <moh2004amed@gmail.com>
2023-09-18 16:50:38 -0700

    hjhj

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * puts_half - check the code
+ * @str: the character
+ * Return: Always 0.
+ */
+void puts_half(char *str)
+{
+int l = 0;
+int half;
+
+while (str[l] != '\0')
+{
+l++;
+half = l / 2;
+if (l % 2 == 1)
+{
+half++;
+_putchar (str[l]);
+}
+while (half < l)
+half++;
+}
+_putchar('\n');
+}
