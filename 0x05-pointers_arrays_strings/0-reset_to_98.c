9a56c5305be4dcde41c8ffe0e7a0b3efaa79e043
deathstroke-Assassin <moh2004amed@gmail.com>
2023-09-18 13:42:47 -0700

    mmm

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,13 +2,9 @@
 /**
  * void reset_to_98 - check the code
  * @n: the integer
- * @p: the pointer
  * Return: Always 0.
  */
 void reset_to_98(int *n)
 {
-int *p;
-
-*p = &n;
-*p = 98;
+*n = 98;
 }
