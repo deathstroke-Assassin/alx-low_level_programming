ea62540ae67028a875dc9404e80749a985f30510
deathstroke-Assassin <moh2004amed@gmail.com>
2023-09-18 18:32:38 -0700

    hgfhgh

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,12 +9,12 @@
 char *_strcpy(char *dest, char *src)
 {
 char *cpy = dest;
-while (src != '\0')
+while (*src != '\0')
 {
 *dest = *src;
 dest++;
 src++;
 }
-*dest ('\0');
-return (*dest);
+*dest = ('\0');
+return (cpy);
 }
