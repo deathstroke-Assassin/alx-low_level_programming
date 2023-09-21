94f99f8f7c764169c36cdfabef76a766ce5ee453
deathstroke-Assassin <moh2004amed@gmail.com>
2023-09-21 15:56:30 -0700

    t0.03

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strncpy - concatenates 2 strings.
+ * @dest: string with concatenation
+ * @src: string to be concatenated
+ * @n: size of the concatenated string
+ * Return: Always 0.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int cpy;
+cpy = 0;
+while (src[cpy] != '\0' && cpy < n)
+{
+*(dest + cpy) = *(src + cpy);
+cpy++;
+}
+*dest = '\0';
+return (dest);
+}
