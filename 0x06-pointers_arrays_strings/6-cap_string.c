ecb363a793e7400a653fa8bcd8be02eb1e2ca3ff
deathstroke-Assassin <moh2004amed@gmail.com>
2023-09-21 18:07:42 -0700

    t0.6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * string_toupper - reverse an arrray.
+ * @str: lowercased string
+ * Return: 0
+ */
+char *string_toupper(char *str)
+{
+	int f;
+	int ch = 0;
+	int a[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+
+	while (*(str + ch) != '\0')
+	{
+		if (*(str + ch) >= 'a' && *(str + ch) <= 'z')
+		{
+			if (ch == 0)
+			{
+				*(str + ch) = *(str + ch) - 32;
+			}
+			else
+			{
+				for (f = 0 ; f <= 12; f++)
+				{
+					if (a[f] == *(str + ch - 1))
+					{
+					*(str + ch) = *(str + ch) - 32;
+					}
+				}
+			}
+		}
+	ch++;
+	}
+return (str);
+}
