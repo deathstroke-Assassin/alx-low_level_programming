ee11dcd50eda68f15f0bbdc91374cad7db8cd0bd
deathstroke-Assassin <moh2004amed@gmail.com>
2023-09-26 00:14:24 -0700

    h

---
+++ 0x07-pointers_arrays_strings/0x07-pointers_arrays_strings/1-memcpy.c~
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _memset - fills memory with a constant byte.
+ * @s: char
+ * @b: char
+ * @n: char
+ * Return: A pointer to the filled memory area @s.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+char *os = s;
+while (n--)
+{
+*s++ = b;
+}
+return (os);
+}
--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,13 +1,29 @@
 #include "main.h"
 /**
+<<<<<<< HEAD
  * _memcpy - fills memory with a constant byte.
  * @src: char
  * @dest: char
+=======
+<<<<<<< HEAD
+ * _memcpy - fills memory with a constant byte.
+ * @src: char
+ * @dest: char
+=======
+ * _memset - fills memory with a constant byte.
+ * @s: char
+ * @b: char
+>>>>>>> e4024f0e22692a9e4024c95c8688f87ca17361eb
+>>>>>>> 25ed3288e9ab982653ffc90d085b5ff4d0979aaf
  * @n: char
  * Return: A pointer to the filled memory area @s.
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+>>>>>>> 25ed3288e9ab982653ffc90d085b5ff4d0979aaf
 unsigned int i = 0;
 
 	while (i < n)
@@ -16,4 +32,15 @@ unsigned int i = 0;
 		i++;
 	}
 return (dest);
+<<<<<<< HEAD
+=======
+=======
+char *od = dest;
+while (n--)
+{
+*src++ = od;
+}
+return (od);
+>>>>>>> e4024f0e22692a9e4024c95c8688f87ca17361eb
+>>>>>>> 25ed3288e9ab982653ffc90d085b5ff4d0979aaf
 }
