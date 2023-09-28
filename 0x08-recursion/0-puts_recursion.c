ef7a70beb14646f4e3a1082aeb008b60d26ea0e8
deathstroke-Assassin <moh2004amed@gmail.com>
2023-09-28 10:23:12 -0700

    1

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts_recursion - print string
+ * @s: char
+ */
+void _puts_recursion(char *s)
+{
+if (s[0] != '\0')
+{
+_putchar(s[0]);
+_puts_recursion(s + 1);
+}
+_putchar('\n');
+}
