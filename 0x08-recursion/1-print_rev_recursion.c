8a7dec68f906bb0b21e875a9a114a47769395b8c
deathstroke-Assassin <moh2004amed@gmail.com>
2023-09-28 12:43:49 -0700

    f

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -5,10 +5,14 @@
  */
 void _print_rev_recursion(char *s)
 {
-if ((s[0] == '\0') || (s[0] < '\0'))
+if (!*s)
 {
-_putchar(s[0]);
-_print_rev_recursion(s - 1);
+return;
+}
+else
+{
+_print_rev_recursion(s + 1);
+_putchar(*s);
 }
 _putchar('\n');
 }
