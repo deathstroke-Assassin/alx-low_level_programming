de2cc33ba77b7843b5bd74d35852ad7a8ed66d93
deathstroke-Assassin <moh2004amed@gmail.com>
2023-09-28 13:28:29 -0700

    j

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen_recursion - print rev string
+ * @s: char
+ * Return: the l
+ */
+int _strlen_recursion(char *s)
+{
+int len = 0;
+if (*s)
+{
+len++;
+len = len + _strlen_recursion(s + 1);
+}
+return (len);
+}
