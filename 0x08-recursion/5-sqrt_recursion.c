f584a80f03b37dbcde252998a9492728df61e9e9
deathstroke-Assassin <moh2004amed@gmail.com>
2023-09-28 16:08:54 -0700

    h

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _sqrt - print rev string
+ * @x: char too! no no
+ * @y: char too! no no
+ * Return: the l
+ */
+int _sqrt(int x, int y)
+{
+if (x * x == y)
+{
+return (x);
+}
+else if (x * x  > y)
+{
+return (-1);
+}
+return (_sqrt(y, x + 1));
+}
+/**
+ * _sqrt_recursion - print rev string
+ * @n: char too! no no
+ * Return: the l
+ */
+int _sqrt_recursion(int n)
+{
+return (_sqrt(n, 1));
+}
