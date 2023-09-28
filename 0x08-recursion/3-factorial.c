b04a64f413376ad92dc7e4020df3cffd42934d2d
deathstroke-Assassin <moh2004amed@gmail.com>
2023-09-28 14:01:32 -0700

    j

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * factorial - print rev string
+ * @n: char
+ * Return: the l
+ */
+int factorial(int n);
+{
+if (n >= 0)
+{
+n = n * factorial(n - 1);
+return (n);
+}
+else if (n >= 0 && n <= 1)
+{
+return (1);
+}
+else if (n < 0)
+{
+return (-1);
+}
