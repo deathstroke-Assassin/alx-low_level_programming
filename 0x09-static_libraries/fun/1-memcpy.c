991fa7540b0fb888f0567982dfd47be1abb4322a
deathstroke-Assassin <moh2004amed@gmail.com>
2023-10-02 08:50:41 -0700

    jkj

--- 0x09-static_libraries/fun/1-memcpy.c
+++ 0x09-static_libraries/fun/1-memcpy.c
@@ -1,46 +1,23 @@
 #include "main.h"
+
 /**
-<<<<<<< HEAD
- * _memcpy - fills memory with a constant byte.
- * @src: char
- * @dest: char
-=======
-<<<<<<< HEAD
- * _memcpy - fills memory with a constant byte.
- * @src: char
- * @dest: char
-=======
- * _memset - fills memory with a constant byte.
- * @s: char
- * @b: char
->>>>>>> e4024f0e22692a9e4024c95c8688f87ca17361eb
->>>>>>> 25ed3288e9ab982653ffc90d085b5ff4d0979aaf
- * @n: char
- * Return: A pointer to the filled memory area @s.
+ * _memcpy - This is my funcion copy Src to dest
+ * @dest: This is my result
+ * @src: This is my string
+ * @n: This is n my number bytes to copy
+ *
+ * Return: This my my string copy
  */
+
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 25ed3288e9ab982653ffc90d085b5ff4d0979aaf
-unsigned int i = 0;
+	unsigned int a;
+	char *pDst = dest;
+	char const *pSrc =  src;
 
-	while (i < n)
+	for (a = 0; a < n; a++)
 	{
-		*(dest + i) = *(src + i);
-		i++;
+		*pDst++ = *pSrc++;
 	}
-return (dest);
-<<<<<<< HEAD
-=======
-=======
-char *od = dest;
-while (n--)
-{
-*src++ = od;
-}
-return (od);
->>>>>>> e4024f0e22692a9e4024c95c8688f87ca17361eb
->>>>>>> 25ed3288e9ab982653ffc90d085b5ff4d0979aaf
+	return (dest);
 }
