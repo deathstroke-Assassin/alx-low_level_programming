145ed57ca8bbf15e08a42b79bc0734ba0369404c
deathstroke-Assassin <moh2004amed@gmail.com>
2023-10-04 07:56:24 -0700

    je

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,43 +1,40 @@
-#include <stdio.h>
-#include "main.h"
 #include <stdlib.h>
-#include <string.h>
+#include "main.h"
+
 /**
- * alloc_grid - creates an array of chars
- * @width: size of array.
- * @height: size of array.
- * Description:  function that creates an array of chars, and initializes it
- * with a specific char.
- * Return: first char.
+ * alloc_grid -  2 dimensional array
+ * @width: width of array
+ * @height: height of array
+ *
+ * Description: function that returns a pointer to a 2 dimensional array of
+ * integers.
+ * Return: NULL or array.
  */
 int **alloc_grid(int width, int height)
 {
-char a[][];
-int i;
-int u;
+	int **arr;
+	int i;
+	int j;
 
-a = (int **)malloc(sizeof(int) * height);
-if (!a || height <= 0 || width <= 0)
-{
-return (NULL);
-}
-while (++i < height)
-{
-*(a + i) = (int *)malloc(sizeof(int) * width);
-if (!(a + i);
-{
-free(a);
-for (j = 0; j <= i; j++)
-{
-free(*(a + i));
-}
- return (NULL)
-}
-}
-for (i = 0; i < height; i++)
+	i = -1;
+	arr = (int **) malloc(sizeof(int *) * height);
+	if (!arr || height <= 0 || width <= 0)
+		return (NULL);
+	while (++i < height)
+	{
+		*(arr + i) = (int *) malloc(sizeof(int) * width);
+		if (!*(arr + i))
+		{
+			free(arr);
+			for (j = 0; j <= i; j++)
+				free(*(arr + j));
+			return (NULL);
+		}
+	}
+	for (i = 0; i < height; i++)
 	{
 		for (j = 0; j < width; j++)
-			a[i][j] = 0;
+			arr[i][j] = 0;
 	}
-return (a);
+	return (arr);
 }
