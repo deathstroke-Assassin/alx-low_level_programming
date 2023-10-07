38ae4dd9bde5de91b1672771ed43de19df85d3ec
deathstroke-Assassin <moh2004amed@gmail.com>
2023-10-07 15:20:56 -0700

    n

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,12 +3,13 @@
 /**
  * malloc_checked - checkt
  * @b: integer to test
+ * Return: pt or 98
  */
 void *malloc_checked(unsigned int b)
 {
 unsigned int *pt;
-*pt = malloc(b);
-if (!)
+pt = malloc(b);
+if (!pt)
 {
 exit(98);
 }
