470da941af99a306bc6b80e63c8559dacaa401ac
deathstroke-Assassin <moh2004amed@gmail.com>
2023-10-08 02:13:08 -0700

    vvvv

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * *_realloc - checkt
+ * @ptr: integer to test
+ * @old_size: size old
+ * @new_size: size new
+ * Return: pt or 98
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+int *ralloc;
+int z;
+if (new_size == old_size)
+{
+return (ptr);
+}
+if (new_size <= 0 && ptr)
+{
+free(ptr);
+return (NULL);
+}
+if (!ptr)
+{
+ralloc =  malloc(new_size);
+if (!ralloc)
+{
+return (NULL);
+}
+}
+z = -1;
+if (new_size > old_size && ptr)
+{
+ralloc = malloc(new_size);
+if (!ralloc)
+return (NULL);
+while (z < new_size)
+{
+while (++z < old_size)
+((char *)ralloc)[z] = ((char *)ptr)[z];
+((char *)ralloc)[z] = 0;
+}
+free(ptr);
+}
+return (ralloc);
+}
