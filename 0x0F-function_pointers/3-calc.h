27bdc77e0d495ed005208c015530dfa430d8908d
deathstroke-Assassin <moh2004amed@gmail.com>
2023-10-12 04:59:41 -0700

    f

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,8 +1,11 @@
-#ifndef XALX
-#define XALX
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
+#ifndef _CALC_H_
+#define _CALC_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
 /**
  * struct op - Struct op
  *
@@ -11,14 +14,14 @@
  */
 typedef struct op
 {
-    char *op;
-    int (*f)(int a, int b);
-} op_t;
+	char *op;
+	int (*f)(int a, int b);
+}		op_t;
 
-op_add(int a, int b);
-op_sub(int a, int b);
-op_mul(int a, int b);
-op_div(int a, int b);
-op_mod(int a, int b);
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
 #endif
