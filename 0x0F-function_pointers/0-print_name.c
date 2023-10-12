41633f9e794b739630c6b9792c7fea6ab4964f56
deathstroke-Assassin <moh2004amed@gmail.com>
2023-10-12 02:42:58 -0700

    cc

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,15 @@
+#include <stddef.h>
 #include "function_pointers.h"
-/**
- * print_name - print
- * @name: h
- * @f: - f
- * Return: 0
- */
 
+/**
+ * print_name - Print name
+ * @name: name that can be printed
+ * @f: ptr of func
+ *
+ * Description:  func to print name using ptr of func
+ */
 void print_name(char *name, void (*f)(char *))
 {
-if (f && name)
-{
-(*f)(name);
-}
+	if (name && f)
+		(*f)(name);
 }
