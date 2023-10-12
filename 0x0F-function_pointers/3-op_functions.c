2f7a309c5de50de11237dc8f827100007c97e93d
deathstroke-Assassin <moh2004amed@gmail.com>
2023-10-12 04:51:42 -0700

    vh

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -3,54 +3,76 @@
 #include <stdlib.h>
 
 /**
- * op_add - add
- * @a: a
- * @b: b
- * Return: res
+ * op_add - addition.
+ * @a: nbr1.
+ * @b: nbr2.
+ *
+ * Description: function that add 2 int.
+ * Return: addition result.
  */
-
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
 /**
- * op_sub - s
- * @a: a
- * @b: b
- * Return: res
+ * op_sub - sub.
+ * @a: nbr1.
+ * @b: nbr2.
+ *
+ * Description: function that substract 2 int.
+ * Return: sub result.
  */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
+
 /**
- * op_div - add
- * @a: a
- * @b: b
- * Return: res
- */
-int op_div(int a, int b)
-{
-	return (a / b);
-}
-/**
- * op_mul - add
- * @a: a
- * @b: b
- * Return: res
+ * op_mul - mul.
+ * @a: nbr1.
+ * @b: nbr2.
+ *
+ * Description: function that multiple 2 int.
+ * Return: mul result.
  */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
+
 /**
- * op_mod - add
- * @a: a
- * @b: b
- * Return: res
+ * op_div - div.
+ * @a: nbr1.
+ * @b: nbr2.
+ *
+ * Description: function that divide 2 int.
+ * Return: div result.
+ */
+int op_div(int a, int b)
+{
+	if (!b)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+
+/**
+ * op_mod - mod.
+ * @a: nbr1.
+ * @b: nbr2.
+ *
+ * Description: function that calculate modulos of 2 int.
+ * Return: mod result.
  */
 int op_mod(int a, int b)
 {
+	if (!b)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a % b);
 }
