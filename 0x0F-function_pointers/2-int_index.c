4a4435454084e4c0f762567a5b7f2e4fe11a8185
deathstroke-Assassin <moh2004amed@gmail.com>
2023-10-12 03:45:36 -0700

    oc

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,28 +1,25 @@
 #include "function_pointers.h"
 
 /**
- * int_index - Print name
- * @array: 1
- * @size: 9
- * @cmp: 7
+ * int_index - search for int.
+ * @array: array to play with.
+ * @size: size of arr.
+ * @cmp: ptr func to action on arr.
  *
- * Return:  fun no
+ * Description: function that searches for an integer.
+ * Return: found the int return 1 or 0.
  */
-
 int int_index(int *array, int size, int (*cmp)(int))
 {
-size_t z;
-z = -1;
-if (size <= 0 || !cmp || !array)
-{
-return (-1);
-}
-while (++z < size)
-{
-if ((*cmp)(array[z]))
-{
-return (z);
-}
-}
-return (-1);
+	int i;
+
+	i = -1;
+	if (size <= 0 || !array || !cmp)
+		return (-1);
+	while (++i < size)
+	{
+		if ((*cmp)(array[i]))
+			return (i);
+	}
+	return (-1);
 }
