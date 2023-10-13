5e8afdddc59e59937289d908b5751feffd1b9e4b
deathstroke-Assassin <moh2004amed@gmail.com>
2023-10-13 08:19:11 -0700

    i

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,14 +9,16 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-if (n == 0)
-	return (0);
 va_list lst;
 unsigned int x, sum;
-va_start(lst, n);
-for (x = 0; x < sum; x++)
+if (n == 0)
 {
-	sum = (sum + va_arg(lst, const unsigned int));
+        return (0);
+}
+va_start(lst, n);
+for (x = 0; x < n; x++)
+{
+	sum += va_arg(lst, const unsigned int);
 }
 va_end(lst);
 return (sum);
