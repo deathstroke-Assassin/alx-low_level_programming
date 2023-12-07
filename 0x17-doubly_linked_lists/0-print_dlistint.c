46104fd517c93e5375d0d9523520ba77df57e3d0
deathstroke-Assassin <moh2004amed@gmail.com>
2023-12-07 09:57:59 -0800

    fgfg

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,4 +1,9 @@
 #include"lists.h"
+/**
+ * print_dlistint - prints doubly linked list int
+ * @h: head of the list
+ * Return: the number of nodes
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
 	int c = 0;
