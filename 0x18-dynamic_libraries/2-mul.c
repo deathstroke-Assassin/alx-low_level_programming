0c1aa554c3da5e604d1f825c0ecaea67bfba3ace
deathstroke-Assassin <moh2004amed@gmail.com>
2023-12-11 12:58:35 -0800

    jkjk

---
+++ 0x18-dynamic_libraries/2-mul.c
@@ -0,0 +1,13 @@
+#include"main.h"
+/**
+ * mul - checks for uppercase character
+ * @a: the character to be checked
+ * @b: the character to be checked
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int mul(int a, int b)
+{
+int c;
+c = a * b;
+return (c);
+}
---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - checks for character
+ * @x: the number to check
+ * Return: 1 if c is letter, 0 otherwise
+ */
+int _abs(int x)
+{
+if (x < 0)
+{
+return (-x);
+}
+return (x);
+}
