397b9e5152ee67d2ce43925b1cec9b818fa73e65
deathstroke-Assassin <moh2004amed@gmail.com>
2023-12-24 04:29:58 -0800

    j js

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,11 +1,13 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - creates a hash table
- * @size: size of the array
+ * key_index - gives the index of a key
+ * @key: key to get index for
+ * @size: size of the hash table
  *
- * Return: pointer to the newly created hash table
+ * Return: index for the key
  */
+
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	return (hash_djb2(key) % size);
