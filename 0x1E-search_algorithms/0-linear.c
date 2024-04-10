0827a0a82b66b0449ac5b5ab15f95e31ff1a5d43
deathstroke-Assassin <moh2004amed@gmail.com>
2024-04-10 10:10:57 -0700

    c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "search_algos.h"
-
+/**
+ * linear_search - search
+ * Return: the target value index
+ * @array: the array of int
+ * @size: the n of int
+ * @value: the value to search for
+ */
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
-	for (i = 0; i < size; i++){
-		if (array[i] == value){
-			return i;
+
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] == value)
+		{
+			return (i);
 		}
 	}
-	return -1;
+	return (-1);
 }
