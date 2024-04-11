7c7ad3fce92bfdf7ff550b347dfe7de8886f6219
deathstroke-Assassin <moh2004amed@gmail.com>
2024-04-11 11:56:27 -0700

    kill

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,5 +1,7 @@
 #include "search_algos.h"
-
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
 /**
  * recursive_search - searches for a value in an array of
  * integers using the Binary search algorithm
